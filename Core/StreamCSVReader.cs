5393709aa6d37e091d06dd0384896c8be7c02921
Nicholas Kamper <nick@nick125.com>
2013-09-27 15:31:22 -0400

    Created CSV parser and added a basic CLI test for it
    Signed-off-by: Nicholas Kamper <nick@nick125.com>

--- Core/StreamCSVReader.cs
+++ Core/StreamCSVReader.cs
@@ -1,6 +1,102 @@
-﻿namespace FuzzyLogic.TGMCProject.Core
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Text;
+
+namespace FuzzyLogic.TGMCProject.Core
 {
-    class StreamCSVReader
+    public class StreamCSVReader
     {
+        private readonly TextReader _reader;
+        private readonly bool _hasHeaders;
+        private readonly Stream _inputStream;
+
+        private bool _hitEol;
+        private bool _hitEof;
+        //private int _position = 0;
+        //private char[] _buffer;
+
+        public StreamCSVReader(Stream inputStream, bool hasHeaders=false)
+        {
+            _hasHeaders = hasHeaders;
+            _inputStream = inputStream;
+            _reader = new StreamReader(inputStream, true);
+
+            _inputStream.Seek(0, SeekOrigin.Begin);
+
+            if (_hasHeaders) ReadHeader();
+        }
+
+        private void ReadHeader()
+        {
+            if (!_hasHeaders) return;
+
+            _inputStream.Seek(0, SeekOrigin.Begin);
+
+            IList<String> headerList = new List<string>();
+
+            while (HasChunkInRecord())
+            {
+                headerList.Add(ReadChunk<String>());
+            }
+        }
+
+        public bool HasChunkInRecord()
+        {
+            return !_hitEol && !_hitEof;
+        }
+
+        public bool NextRecord()
+        {
+            if (_hitEof) return false;
+            _hitEol = false;
+
+            return true;
+        }
+
+        /**
+         * Reads a chunk of data and tries to parse it as a specific type
+         */
+        public T ReadChunk<T>() where T : IConvertible
+        {
+            var readChunk = false;
+            var st = new StringBuilder();
+
+            while (!readChunk)
+            {
+                // Fill the buffer
+                var c = _reader.Read();
+
+                if (c < 0)
+                {
+                    _hitEof = true;
+                    break;
+                }
+
+                switch ((char)c)
+                {
+                    case '\r':
+                        break;
+                    case '\n':
+                    case '\0':
+                        _hitEol = true;
+                        readChunk = true;
+                        break;
+                    case '"':
+                    case '\'':
+                        break;
+                    case ',':
+                        readChunk = true;
+                        break;
+                    default:
+                        st.Append((char)c);
+                        break;
+                }
+            }
+
+            var converter = TypeDescriptor.GetConverter(typeof (T));
+            return st.Length > 0 ? (T) converter.ConvertFromString(st.ToString()) : default(T);
+        }
     }
 }
