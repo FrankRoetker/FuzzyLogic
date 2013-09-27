8932e98825332a2ea64d00547970eac41096bc7e
Nicholas Kamper <nick@nick125.com>
2013-09-27 15:40:17 -0400

    Added unittests
    Signed-off-by: Nicholas Kamper <nick@nick125.com>

---
+++ UnitTests/ExampleTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace FuzzyLogic.UnitTests
+{
+    [TestFixture]
+    public class ExampleTest
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            Assert.IsTrue("foo" == "foo");
+        }
+    }
+}
