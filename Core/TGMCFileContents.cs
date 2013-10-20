117bedfb1bcdca7ceb9e11037818f43c827a7aee
lauramossity <lauramossity@gmail.com>
2013-10-20 01:06:02 -0400

    initial commit for logistic regression

---
+++ Core/TGMCFileContents.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuzzyLogic.TGMCProject.Core
+{
+    class TGMCFileContents
+    {
+        // TODO: Make an internal representation of a file's contents. Include support for 
+        // getting counts of a particular value in a column
+    }
+}
