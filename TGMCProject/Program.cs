7e7ecacf1da387174cda00bee73fc405b5a337bc
dochaven <roetkefj@rose-hulman.edu>
2013-10-23 21:53:36 -0400

    I'm sorry.

--- TGMCProject/Program.cs
+++ TGMCProject/Program.cs
@@ -11,9 +11,9 @@ namespace FuzzyLogic.TGMCProject
         {
             var classifier = new LogisticRegressionClassifier();
 
-            //var reader = new StreamCSVReader(new System.IO.FileStream("..\\..\\..\\Datasets\\q1repeatedtrain.csv", System.IO.FileMode.Open), enableQuotes:false);
+            var reader = new StreamCSVReader(new System.IO.FileStream("..\\..\\..\\Datasets\\q1repeatedtrain.csv", System.IO.FileMode.Open), enableQuotes: false);
 
-            //classifier.TrainClassifier(reader);
+            classifier.TrainClassifier(reader);
 
             var svm = new SVM("..\\..\\..\\Datasets\\q1train.csv", "..\\..\\..\\Datasets\\tgmcevaluation.csv");
             svm.SvmSolver();
