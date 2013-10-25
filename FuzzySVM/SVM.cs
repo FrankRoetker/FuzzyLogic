bc95e715fe2846617ecdad54daa282f84d9fb508
Frank Roetker <roetkefj@rose-hulman.edu>
2013-10-24 23:05:58 -0400

    this is at least giving us values...
    even though they are all wrong. :/ It is going to take a lot of tweaking the gamma and C values to get some answers that are not terrible.

--- FuzzySVM/SVM.cs
+++ FuzzySVM/SVM.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using FuzzySVM;
 using libsvm;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace FuzzyLogic.TGMCProject.FuzzySVM
 {
@@ -8,22 +10,27 @@ namespace FuzzyLogic.TGMCProject.FuzzySVM
     {
         public const double FEATURES = 300.0;
 
-        private svm_problem prob;
-        private svm_problem test;
+        private PersonalProblem prob;
+        private PersonalProblem test;
 
         public SVM(string trainingFile, string testFile)
         {
-            prob = FuzzyProblemHelper.ReadAndScaleProblem(trainingFile);
-            test = FuzzyProblemHelper.ReadAndScaleProblem(testFile, testing: false);
+            var throwaway = new List<FuzzyProblemHelper.temp>();
+            var Data = new List<FuzzyProblemHelper.temp>();
+
+            prob = FuzzyProblemHelper.ReadAndScaleProblem(trainingFile, out throwaway);
+            test = FuzzyProblemHelper.ReadAndScaleProblem(testFile, out Data, testing: false);
         }
 
-        public void SvmSolver()
+        public List<int> SvmSolver()
         {
             //double gamma = width parameter
             //double C = Cost parameter
             //int nr_fold = ??
-            double gamma = 0.5;
-            double C = 1.0/FEATURES;
+            double gamma = 0.7;
+            double C = 0.3; //1.0/FEATURES;
+
+            var stuff = new List<int>();
 
 
             var svm = new C_SVC(prob, KernelHelper.RadialBasisFunctionKernel(gamma), C); 
@@ -37,11 +44,12 @@ namespace FuzzyLogic.TGMCProject.FuzzySVM
 
                 if (predict.CompareTo(1.0) == 0)
                 {
-                    //we found an answer!
-                    Console.WriteLine(test.y[i]);
+                    //we found an answer
+                    stuff.Add(test.answerId[i]);
                 }
             }
-        }
 
+            return stuff.ToList();
+        }
     }
 }
